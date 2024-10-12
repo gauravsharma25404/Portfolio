068d8abeddb06fbf3f094c12fe828328559d4661
Gaurav <gauravsharma25404@gmail.com>
2024-10-12 10:17:27 +0530

    removing unsed codes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,12 +4,12 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 
-import Button from "react-bootstrap/Button";
+// import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
-import { CgGitFork } from "react-icons/cg";
+// import { CgGitFork } from "react-icons/cg";
 
 import {
-  AiFillStar,
+  // AiFillStar,
   AiOutlineHome,
   AiOutlineFundProjectionScreen,
   AiOutlineUser,
